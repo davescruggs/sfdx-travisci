5d7050aa1a420b017a815358feed6af2ae794291
Dave Scruggs <davescruggs84@gmail.com>
2017-06-29 10:30:49 -0700

    Updated YAML for my dev hub and github repo

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export CONSUMER_KEY=3MVG9zlTNB8o8BA3Uh2oNtIey1olle0bjhXYT7t6VG7KfVMqf41LM6URtO7rDV18saoGsIEOx2nmV5U6JGBco
+export JWT_KEY_FILE=/Users/dscruggs/SFDemo/SFDX/sfdx-travisci/certificates/server.key
+export HUB_USERNAME=dscruggs@dx2017.mds
