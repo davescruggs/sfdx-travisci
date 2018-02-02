3e56fe3af59c5d6d7884bbdaa7b9a02b5fd8f904
David Scruggs <dscruggs@dscruggs-ltm.internal.salesforce.com>
2018-02-02 14:05:29 -0500

    initial commit with added tests

---
+++ spec/test/jasminetest.js
@@ -0,0 +1,8 @@
+/**
+ * This is a 'hello world' Jasmine test spec
+ */
+describe("A simple passing test", function() {
+    it("checks that true is always true", function() {
+        expect(true).toBe(true);
+    });
+});
